0f7e1559b6f4483f790fe0579ce95aface8017db
KarinyCCTB <kariny.marques4878@mycanctb.ca>
2024-11-27 23:12:17 -0800

    Update testing file to use modules

--- tests/test_testing.js
+++ tests/test_testing.js
@@ -1,19 +1,17 @@
-const {Builder, By, Key, until, Capabilities} = require('selenium-webdriver');
-const firefox = require('selenium-webdriver/firefox');
-const chai = require('chai');
-const assert = chai.assert;
+import { Builder, By, Key, until, Capabilities } from 'selenium-webdriver';
+import { Options } from 'selenium-webdriver/chrome.js'; 
+import { assert } from 'chai'; 
 
 let driver;
 
-// Start browser before each test
 before(async () => {
-    const options = new firefox.Options();
-    options.addArguments('-headless');
-    options.addArguments('window-size=1200x600'); // Set window size for headless mode
+    const options = new Options();
+    options.addArguments('--headless');
+    options.addArguments('window-size=1200x600');
 
     driver = await new Builder()
-        .forBrowser('firefox')
-        .setChromeOptions(options)
+        .forBrowser('chrome') 
+        .setChromeOptions(options) 
         .build();
 });
 
